46c6dd452129eff426d3e7a742a526518fd1e029
Vitalii <vetalyathebest2009@gmail.com>
2022-07-13 14:15:04 +0300

    first commit leson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://i.pinimg.com/474x/d3/0a/19/d30a19c7f5c494218deb98202f3f6422.jpg'></img>
+  </header>
+} 
+
+export default Header; 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return       <div className='content'>
+    <div>
+    <img src='https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New posts
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  </div>
+
+} 
+
+export default Profile; 
